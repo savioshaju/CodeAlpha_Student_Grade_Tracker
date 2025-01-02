d640eaab296a0e90bf7b8f8244ee84653b1d5f96
savioshaju <81824615+savioshaju@users.noreply.github.com>
2025-01-02 12:15:18 +0530

    Update Student_Grade_Tracker.java

--- Student_Grade_Tracker.java
+++ Student_Grade_Tracker.java
@@ -30,7 +30,7 @@ public class Student_Grade_Tracker {
                 high = score[i];
         }
         avgm = (float) t / n;
-        System.out.println(""Average Mark: " + avgm);
+        System.out.println("Average Mark: " + avgm);
         System.out.println("Highest Mark: " + high);
         System.out.println("Lowest Mark: " + low);
     }
