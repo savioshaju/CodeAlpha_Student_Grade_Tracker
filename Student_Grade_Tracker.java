247f2e99e5257d20b906200482298afd484c7a1e
savioshaju <81824615+savioshaju@users.noreply.github.com>
2025-01-02 12:03:15 +0530

    Add files via upload

---
+++ Student_Grade_Tracker.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Student_Grade_Tracker {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number Of Students");
+        int n = sc.nextInt();
+        int score[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            int mark;
+            while (true) {
+                System.out.println("Enter The mark (out of 100) of Student " + (i + 1) + ":");
+                mark = sc.nextInt();
+                if (mark >= 0 && mark <= 100) {
+                    score[i] = mark;
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter a valid mark between 0 and 100.");
+                }
+            }
+        }
+        int high = 0, low = 100, t = 0;
+        float avgm;
+        for (int i = 0; i < n; i++) {
+            t = t + score[i];
+            if (score[i] < low)
+                low = score[i];
+            if (score[i] > high)
+                high = score[i];
+        }
+        avgm = (float) t / n;
+        System.out.println("Avrage Mark :" + avgm);
+        System.out.println("Highest Mark: " + high);
+        System.out.println("Lowest Mark: " + low);
+    }
+}
